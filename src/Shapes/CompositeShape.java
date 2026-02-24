32c6ddb9dbb22bde36654185b554aa68f6c61a00
Tzaga <imrantzaga@gmail.com>
2026-02-24 17:53:23 +0100

    fixat composite.
    TODO: Fixa så att alla shapes följer move och resize och moveto osv i CompositeShape

---
+++ src/Shapes/CompositeShape.java
@@ -0,0 +1,95 @@
+package Shapes;
+
+import java.awt.*;
+import java.util.List;
+
+public class CompositeShape implements Shape {
+
+    private Point center;
+    private double width, height;
+    private boolean marked;
+    private List<Shape> shapesList;
+
+    public CompositeShape(double x, double y, double width, double height, List<Shape> shapesList)
+    {
+        center = new Point(x,y);
+        this.width = width;
+        this.height = height;
+        this.marked = false;
+        this.shapesList = shapesList;
+
+    }
+
+    public CompositeShape(Point point, double width, double height, List<Shape> shapesList)
+    {
+        this(point.getX(), point.getY(),width,height, shapesList);
+        this.marked = false;
+    }
+
+    @Override
+    public void draw(Graphics g)
+    {
+        g.setColor(Color.black);
+        shapesList.forEach(shape -> shape.draw(g));
+        g.drawRect((int)(center.getX()), (int)(center.getY()), (int)width, (int)height);
+    }
+
+    @Override
+    public Point getPosition()
+    {
+        return center;
+    }
+
+    @Override
+    public double getWidth()
+    {
+        return this.width;
+    }
+
+    @Override
+    public double getHeight()
+    {
+        return this.height;
+    }
+
+    @Override
+    public boolean intersects(Point point)
+    {
+        return center.distanceTo(point) < width/2.0;
+    }
+
+    @Override
+    public void moveTo(Point point)
+    {
+        center.moveTo(point);
+    }
+
+    @Override
+    public void move(double dx, double dy)
+    {
+        center.move(dx, dy);
+    }
+
+    @Override
+    public void resizeTo(Point point)
+    {
+        this.width = Math.abs(point.getX() - center.getX()) * 2.0;
+        this.height = Math.abs(point.getY() - center.getY()) * 2.0;
+    }
+
+    @Override
+    public Shape peel()
+    {
+        return this;
+    }
+
+    @Override
+    public void setMarked(boolean marked) {
+        this.marked = marked;
+    }
+
+    @Override
+    public boolean isMarked() {
+        return this.marked;
+    }
+}
---
+++ src/States/StateMerge.java
@@ -0,0 +1,79 @@
+package States;
+
+import Shapes.CompositeShape;
+import Shapes.Point;
+import Main.ShapeApp;
+import Shapes.Shape;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+public class StateMerge extends State {
+    Point pointDown;
+    List<Shape> shapesOnCanvas;
+
+    public StateMerge(ShapeApp app) {
+        super(app);
+        shapesOnCanvas = app.getShapeContainer().getShapes();
+    }
+
+    @Override
+    public void pointerDown(Point point) {
+        this.pointDown=point;
+
+    }
+
+    /*
+    @Override
+    public void pointerMoved(Point point, boolean pointerDown) {
+        if(app.getShapeContainer().getSelected() != null && pointerDown) {
+            Shape marker = new Rectangle(point.getX(),point.getY(),50,50);
+            app.getShapeContainer().addShape(marker);
+            app.getShapeContainer().setSelected(marker);
+            marker.resizeTo(point);
+            app.getShapeContainer().repaint();
+        }
+    }
+     */
+
+    @Override
+    public void pointerUp(Point point) {
+        // hämta vilka shapes som är inom markeringen
+        // skapa en ny compositeShape
+        // ta bort shapes som sätts in i compositeshape
+        List<Shape> shapesInMerge = mergeShapes(shapesOnCanvas,pointDown,point);
+
+        double x = Math.min(pointDown.getX(), point.getX());
+        double y = Math.min(pointDown.getY(), point.getY());
+        double width = Math.abs(pointDown.getX() - point.getX());
+        double height = Math.abs(pointDown.getY() - point.getY());
+
+        app.getShapeContainer().addShape(new CompositeShape(x, y, width, height, shapesInMerge));
+        System.out.println(app.getShapeContainer().getShapes().size());
+        app.getShapeContainer().repaint();
+    }
+
+    private List<Shape> mergeShapes(List<Shape> list, Point pointDown, Point pointUp) {
+
+        List<Shape> shapesInMerge = new ArrayList<>();
+
+        list.forEach(shape -> {
+            if(shape.getPosition().getX()>pointDown.getX()&&
+                    shape.getPosition().getX()<pointUp.getX()&&
+                    shape.getPosition().getY()>pointDown.getY()&&
+                    shape.getPosition().getY()<pointUp.getY()
+            )
+            {
+                shapesInMerge.add(shape);
+                System.out.println("shape in merge");
+            }
+        });
+
+        app.getShapeContainer().getShapes().removeAll(shapesInMerge);
+
+
+        return shapesInMerge;
+    }
+}
