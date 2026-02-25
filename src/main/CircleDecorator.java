2570d571b654952babaed2d744f5ca3350d80cfe
Isak <isakenglund@hotmail.com>
2026-02-25 22:33:03 +0100

    separerat decorator på circle och rectanle

--- src/main/CircleDecorator.java
+++ src/main/CircleDecorator.java
@@ -13,12 +13,19 @@ public class CircleDecorator extends ShapeDecorator{
     }
 
     @Override
-    public void draw(Graphics g)
-    {
+    public void draw(Graphics g) {
         super.draw(g);
-        Point position = super.getPosition();
-        int x = (int)(position.getX()-super.getWidth()/2.0) ;
-        int y = (int)(position.getY()-super.getHeight()/2.0);
-        g.drawOval(x,y,(int)(super.getWidth())+1,(int)(super.getHeight())+1);
+
+        Point center = super.getPosition();
+        double radius = super.getWidth() / 2.0;
+
+        int padding = 2;
+
+        int x = (int) Math.round(center.getX() - radius) - padding;
+        int y = (int) Math.round(center.getY() - radius) - padding;
+        int w = (int) Math.round(2 * radius) + 2 * padding;
+        int h = (int) Math.round(2 * radius) + 2 * padding;
+
+        g.drawOval(x, y, w, h);
     }
 }
