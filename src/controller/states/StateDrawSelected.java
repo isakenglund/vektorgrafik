56d225f6623ecd8a8aba69adb9b376765e8f21f7
isake <isakenglund@hotmail.com>
2026-03-21 09:04:09 +0100

    får inte till med markerade shapes

--- src/controller/states/StateDrawSelected.java
+++ src/controller/states/StateDrawSelected.java
@@ -1,12 +1,20 @@
 package controller.states;
 
+import model.shapes.Circle;
+import model.shapes.CompositeShape;
 import model.shapes.Point;
 import model.shapes.Shape;
+import model.shapes.style.Style;
+import model.shapes.style.StyleFactory;
 import view.ShapeApp;
+
+import java.awt.*;
+import java.util.ArrayList;
 
 public class StateDrawSelected extends State {
 
     private Point lastMousePosition;
+    private ArrayList<Shape> shapes;
 
     public StateDrawSelected(ShapeApp app) {
         super(app);
@@ -15,11 +23,14 @@ public class StateDrawSelected extends State {
     @Override
     public void pointerDown(Point point) {
         System.out.println("pointer down");
-
         Shape selectedShape = app.getShapeContainer().getSelected();
         System.out.println("selected shape: " + selectedShape);
         if (selectedShape != null){
-            System.out.println("pointer down");
+            lastMousePosition = point;
+        }
+
+        shapes = (ArrayList<Shape>) app.getShapeContainer().getIsMarked();
+        if (!shapes.isEmpty()){
             lastMousePosition = point;
         }
 
@@ -27,6 +38,8 @@ public class StateDrawSelected extends State {
 
     @Override
     public void pointerMoved(Point point, boolean pointerDown) {
+        System.out.println("pointer moved");
+        System.out.println(lastMousePosition);
             if (lastMousePosition != null) {
                 double dx = point.getX() - lastMousePosition.getX();
                 double dy = point.getY() - lastMousePosition.getY();
@@ -35,10 +48,12 @@ public class StateDrawSelected extends State {
                 System.out.println(distance);
 
                 if(distance>10) {
-                    app.getShapeContainer().addShape(app.getShapeContainer().getSelected().clone());
+                    //app.getShapeContainer().addShape(app.getShapeContainer().getSelected().clone());
+                    //app.getShapeContainer().addShape(new Circle(point, 50,50, StyleFactory.getInstance().getStyle(Color.BLACK,1)));
+                    app.getShapeContainer().addShape(new CompositeShape(point, 10,10, shapes,StyleFactory.getInstance().getStyle(Color.BLACK,1) ));
+
                     lastMousePosition = point;
                 }
-
                 app.getShapeContainer().repaint();
             }
     }
@@ -49,6 +64,4 @@ public class StateDrawSelected extends State {
             lastMousePosition = null;
         }
     }
-
-
 }
